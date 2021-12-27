2b84c371531ba166150060de7540f0b9b50a5845
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-27 10:24:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const mentorRouter = require("./Routers/MentorRouter");
+const studentRouter = require("./Routers/StudentRouter");
+
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cors = require("cors");
+app.use(cors()); /* To avoid cross origin error */
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+const URL = process.env.MONGODB_URL;
+
+const mongoose = require("mongoose");
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+const connection = mongoose.connection;
+connection.on("open", () => console.log("MongoDB Connected"));
+
+app.get("/", (req, res) =>
+  res.send(`STUDENT-MENTOR-TASK`)
+);
+
+app.use("/Mentors", mentorRouter);
+app.use("/Students", studentRouter);
+
+app.listen(PORT, () => console.log(`Server started in the port ${PORT}`));
